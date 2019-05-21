8691130924bcad39a7db2754c43dfff4d1c55f7d
Nikolesh <nikolesh27@gmail.com>
2019-05-21 09:14:57 +0000

    swag

--- mono-rent/src/app/app.component.ts
+++ mono-rent/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { user } from './user.model'; // <-- import this
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,15 +12,26 @@ export class AppComponent {
   title = 'mono-rent';
   utente: user[];
   oArt: Observable<user[]>;
+  data = Object;
+
 
 
 constructor(public http: HttpClient) {
     //Fai la get e ottieni la lista di articoli e riempi il vettore articles
     this.utente = new Array<user>();
-    this.oArt = this.http.get<user[]>('https://my-json-server.typicode.com/Nikolesh/server/user');
-    this.oArt.subscribe(this.ricevidati);
+    this.oArt = this.http.get<user[]>('https://my-json-server.typicode.com/Nikolesh/informatica/user');
+
+    this.oArt.subscribe(data => {
+            data.forEach(element => {
+                this.utente.push(new user(element.nome, element.cognome, element.nikname, Number(element.telefono), element.email, element.psw));
+            });
+        })
 
 }
+ngOnInit() {
+        this.utente = new Array<user>();
+    }
+
 reg(nome: HTMLInputElement  ,cognome : HTMLInputElement, nikname :HTMLInputElement, telefono :HTMLInputElement, email :HTMLInputElement, psw :HTMLInputElement  ) : boolean
   {
     this.utente.push(new user(nome.value, cognome.value, nikname.value, Number(telefono.value), email.value, psw.value));
